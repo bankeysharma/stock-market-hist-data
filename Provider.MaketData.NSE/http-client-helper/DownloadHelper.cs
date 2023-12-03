32b1eca6dd91ed513a01d58b43568cf7ef1d141a
Bankey Sharma <bankeysharma@yahoo.com>
2023-12-03 14:16:23 +0530

    Refactoring code for simplification

--- Provider.MaketData.NSE/http-client-helper/DownloadHelper.cs
+++ Provider.MaketData.NSE/http-client-helper/DownloadHelper.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 using System.Net.Mail;
+using System.Security.Policy;
 
 namespace Provider.MaketData.NSE.HttpClientHelper {
     public static class DownloadHelper {
@@ -23,32 +24,38 @@ namespace Provider.MaketData.NSE.HttpClientHelper {
 
             string contentString = string.Empty;
 
-            using (HttpClient client = new HttpClient(new HttpClientHandler {
+            //CookieContainer cookies = SessionCookiesOfNSEIndia;
+
+            HttpClientHandler clientHandler = new HttpClientHandler {
                 CookieContainer = new CookieContainer(),
                 UseCookies = true,
                 UseDefaultCredentials = true,
-                PreAuthenticate = true
+                PreAuthenticate = true,
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
 
-            })) {
-                client.BaseAddress = new Uri(absoluteUriToDownload);
+            };
+
+            using (HttpClient client = new HttpClient(clientHandler)) {
                 client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", string.Empty);
+                RequestHeaders.ForEach(requestHeader => client.DefaultRequestHeaders.Add(requestHeader.Key, requestHeader.Value));
+                MediaTypes.ForEach(mediaType => client.DefaultRequestHeaders.Accept.Add(mediaType));
 
-                HttpResponseMessage response = await client.GetAsync(absoluteUriToDownload);
+                client.Timeout = TimeSpan.FromSeconds(10);
 
-                if (response.IsSuccessStatusCode) {
-                    
-                    //* Get stream of content from downloaded file
-                    HttpContent content = response.Content;
-                    Stream contentStream = await content.ReadAsStreamAsync();
+                using (HttpResponseMessage response = await client.GetAsync(absoluteUriToDownload)) {
 
-                    //* Read stremed content as string
-                    StreamReader reader = new StreamReader(contentStream);
-                    contentString = reader.ReadToEnd();
-                } else {
-                    throw new Exception(response.ToString());
+                    if (response.IsSuccessStatusCode) {
+
+                        //* Get stream of content from downloaded file
+                        HttpContent content = response.Content;
+                        Stream contentStream = await content.ReadAsStreamAsync();
+
+                        //* Read stremed content as string
+                        StreamReader reader = new StreamReader(contentStream);
+                        contentString = reader.ReadToEnd();
+                    } else {
+                        throw new Exception(response.ToString());
+                    }
                 }
             }
 
@@ -75,5 +82,73 @@ namespace Provider.MaketData.NSE.HttpClientHelper {
 
             File.WriteAllText(destinationFileFullPath, contentToWrite);
         }
+
+        #region Helper methods
+
+        private static List<KeyValuePair<string, string>> RequestHeaders {
+            get {
+                return new KeyValuePair<string, string>[] { new KeyValuePair<string, string>("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"),
+                    new KeyValuePair<string, string>("Sec-Fetch-Mode", "navigate"),
+                    new KeyValuePair<string, string>("Sec-Fetch-Dest", "document")
+                 }.ToList();
+            }
+        }
+
+        private static List<MediaTypeWithQualityHeaderValue> MediaTypes {
+            get {
+                return new MediaTypeWithQualityHeaderValue[] { 
+                    new MediaTypeWithQualityHeaderValue("application/json"),
+                    new MediaTypeWithQualityHeaderValue("text/html"),
+                    new MediaTypeWithQualityHeaderValue("application/xhtml+xml"),
+                    new MediaTypeWithQualityHeaderValue("application/xml", 0.9),
+                    new MediaTypeWithQualityHeaderValue("image/webp"),
+                    new MediaTypeWithQualityHeaderValue("image/apng"),
+                    new MediaTypeWithQualityHeaderValue("*/*", 0.8),
+                    new MediaTypeWithQualityHeaderValue("application/signed-exchange", 0.7)
+                }.ToList();
+            }
+        }
+
+        private static CookieContainer SessionCookiesOfNSEIndia {
+            get {
+
+                CookieContainer cookies = new CookieContainer();
+
+                var clientHandler = new HttpClientHandler {
+                    CookieContainer = cookies,
+                    UseCookies = true,
+                    UseDefaultCredentials = true,
+                    PreAuthenticate = true,
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+
+                };
+
+                string uri = @"https://www.nseindia.com/companies-listing/corporate-filings-actions";
+
+                using (HttpClient client = new HttpClient(clientHandler)) {
+                    client.BaseAddress = new Uri(uri);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    RequestHeaders.ForEach(requestHeader => client.DefaultRequestHeaders.Add(requestHeader.Key, requestHeader.Value));
+                    MediaTypes.ForEach(mediaType => client.DefaultRequestHeaders.Accept.Add(mediaType));
+
+                    client.Timeout = TimeSpan.FromSeconds(5);
+
+                    HttpResponseMessage response = client.GetAsync(uri).Result;
+
+                    cookies = clientHandler.CookieContainer;
+
+                    response.Dispose();
+                    client.CancelPendingRequests();
+                    client.Dispose();
+
+                }
+
+                return cookies;
+
+            }
+        }
+
+        #endregion
+
     }
 }
